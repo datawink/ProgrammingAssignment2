a506473b0cb9536c7d20962d8907c3602b7fd99d
datawink <datawink@users.noreply.github.com>
2016-07-30 20:37:45 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Adding a test line
-## Adding a new test line
+## This pair of functions work together to enable the caching of the calculating of the inverst of a matrix.
+## First makeCacheMatrix creates a special type of matrix which caches it's inverse.  cacheSolve can be run to check if the inverse
+## calculation has already been cached.  If so, it returns the cached inverse.  If not, it calculates the inverse and updates the
+## cached inverse in the special makeCacheMatrix matrix  
+
+## 
+## Attributions:  These two functions represent just minor modifcations of the makeVector and cachemean functions created by R. Peng 
+##                for the Coursera Programming in R Course
+##                The coding of this assignment was made possilbe thanks to the excellend explanation of the makeVector and cachemean 
+##                functions posted by Leonard Greski on the disucssion board for this assignment
 
 ## Write a short comment describing this function
 
