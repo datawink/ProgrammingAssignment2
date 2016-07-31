029aee355f8aa398cd92bfb29750bc843de4cf92
datawink <datawink@users.noreply.github.com>
2016-07-30 21:51:49 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,23 @@
 ## This pair of functions work together to enable the caching of the calculation of the inverse of a matrix.
 ## First makeCacheMatrix creates a special type of matrix which caches it's inverse.  Then, cacheSolve can be run to check if the 
-## inverse calculation has already been cached.  If so, (and the matrix hasn't changed) it returns the cached inverse.  
+## inverse calculation has already been cached.  If so, (and the matrix hasn't changed) cacheSolve returns the cached inverse.  
 ## If the inverse has not been calculated, cacheSolve calculates it and updates the cached inverse in the special 
 ## makeCacheMatrix matrix.
 ## 
-## Assumptions:  These functions assumes that the matrix is invertible.
+## Assumptions:  These functions assume that the matrix is invertible.
 
 ## 
 ## Attributions:  These two functions represent just minor modifcations of the makeVector and cachemean functions created by R. Peng 
 ##                for the "Coursera Programming in R Course" Assignment 2.
 ##                The descriptions of what these functions do is largely based on the assignment text written by R.Peng.
-##                The coding of this assignment was made possilbe thanks to the excellend explanation of the makeVector and cachemean 
+##                The coding of this assignment was made possilbe thanks to the excellent explanation of the makeVector and cachemean 
 ##                functions posted by Leonard Greski on the disucssion board for this assignment.
 ##                These code stubs are forked from https://github.com/rpeng/ProgrammingAssignment2
 
 
 
-## makeCacheMatrix is a function which creates a special matrix which caxhes it's onw inverse.
-## This funciton establishes "getters" and "setters" to access both the matrix itself and its inverse
+## makeCacheMatrix is a function which creates a special matrix which caches its own inverse.
+## This funciton establishes "getters" and "setters" to access both the matrix itself and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {   
 
@@ -39,23 +39,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve is function which returns the inverse of 'x'
-## This function can be used on the 'special' matrices created using the makeCacheMatrix funciton above.
+## cacheSolve is a function which returns the inverse of 'x'
+## This function can be used on the 'special' matrices created using the makeCacheMatrix function above.
 ## This function first looks to see if the inverse has already been calculated by the makeCacheMatrix function.
 ## If so, and the matrix hasn't changed, it is retrieved.
-## The x passed to this function is acctually a list of setters and getters into the matrix created by makeCacheMatrix.
+## The x passed to this function is actually a list of setters and getters into the matrix created by makeCacheMatrix.
 ## cacheSolve returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-      i <- x$getInv()                         #Attempt to retrieve the cached inverse
+      i <- x$getInv()                         #Attempt to retrieve the cached inverse.
       
-      if(!is.null(i)) {                       #If the inverse exists (it's not NULL), retrieve it
+      if(!is.null(i)) {                       #If the inverse exists (it's not NULL), retrieve it.
         message("getting cached inverse")
         return(i)
       }
  
-      tempMatrix <- x$get()                   #if the inverse doesn't already exist, we need to calculate it
+      tempMatrix <- x$get()                   #if the inverse doesn't already exist, we need to calculate it.
       i <- solve(tempMatrix,,,,)
       x$setInv(i)
-      i
+      i                                       #return the inverse
 }
